65513c48bae1e8441a07bbe46f76c319eab60484
fgrilli <federico.grilli@gmail.com>
2016-04-05 16:41:08 +0200

    use kebab instead of camel notation for commands. Update readme

--- bin/extractMagnolia.js
+++ bin/extractMagnolia.js
@@ -5,8 +5,8 @@ var fs = require('fs');
 var packageJson = require('../package.json');
 var tomcatFolder = path.normalize(packageJson.setupMagnolia.tomcatFolder);
 
-if(fs.existsSync(tomcatFolder)) {
-    console.log(tomcatFolder+" already exists. We won't extraxt it again.")
+if (fs.existsSync(tomcatFolder)) {
+    console.log(tomcatFolder + " already exists. We won't extract it again.")
     return;
 }
 
@@ -14,13 +14,13 @@ console.log("Extracting ...")
 
 var zip = new AdmZip("magnolia.zip");
 
-var zipEntries = zip.getEntries(); 
- 
-zipEntries.forEach(function(zipEntry) {
-  if (zipEntry.entryName.match(/^(magnolia).*(\/apache-tomcat).*\/$/) && zipEntry.entryName.split('/').length < 4){
-    console.log(zipEntry.entryName,"to",tomcatFolder);
-    if(zip.extractEntryTo(zipEntry.entryName, tomcatFolder, false, false)){
-      console.log("Extraction completed");
+var zipEntries = zip.getEntries();
+
+zipEntries.forEach(function (zipEntry) {
+    if (zipEntry.entryName.match(/^(magnolia).*(\/apache-tomcat).*\/$/) && zipEntry.entryName.split('/').length < 4) {
+        console.log(zipEntry.entryName, "to", tomcatFolder);
+        if (zip.extractEntryTo(zipEntry.entryName, tomcatFolder, false, false)) {
+            console.log("Extraction completed");
+        }
     }
-	}
 });
