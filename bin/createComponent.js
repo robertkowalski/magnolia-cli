67b1085064fdd026759768085a7dc2c0cd68d4f3
Aleksandr Pchelintcev <aleksandr.pchelintcev@magnolia-cms.com>
2016-04-22 13:31:44 +0300

    NPMCLI-4 refactor addAvailability.js and createComponent.js to use the new YAML manipulation utility

--- bin/createComponent.js
+++ bin/createComponent.js
@@ -2,34 +2,29 @@
 
 var path = require('path');
 var fs = require('fs');
-var fse = require('fs.extra');
 var createFromPrototype = require('./createFromPrototype');
+var argumentParser = require('./argumentParser');
 var addAvailability = require('./addAvailability');
-
 var packageJson = require('../package.json');
-
-var prototypesFolder = "./_prototypes";
-
 var userArgs = process.argv.slice(2);
 var newComponentName = userArgs[0];
-var availabilityPatern = userArgs[1];
+var availabilityPattern = userArgs[1];
 
 var createComponent = function() {
 	if(newComponentName){
 		
 		var templateDefinitionFile = packageJson.config.outputPath + "/"+ packageJson.lightModuleName + packageJson.lightDevFoldersInModule.templates_components + "/" + newComponentName + ".yaml";
-		var templateDefinitionId = packageJson.lightModuleName +":"+ packageJson.lightDevFoldersInModule.templates_components.replace("/templates/","") + "/" + newComponentName;
 		var templateScriptFile = packageJson.config.outputPath + "/"+ packageJson.lightModuleName + packageJson.lightDevFoldersInModule.templates_components + "/" + newComponentName + ".ftl";
 		var dialogDefinitionFile = packageJson.config.outputPath + "/"+ packageJson.lightModuleName + packageJson.lightDevFoldersInModule.dialogs_components + "/" + newComponentName + ".yaml";
 		var dialogDefinitionId = packageJson.lightModuleName +":"+ packageJson.lightDevFoldersInModule.dialogs_components.replace("/dialogs/","") + "/" + newComponentName;
 		
 		// component definition
 		if(fs.existsSync(path.normalize(templateDefinitionFile))) {
-			console.log("WARN: ","'"+newComponentName+"' component template already exists");
+			console.log("WARN: ","'" + newComponentName + "' component template already exists");
 		} else {
-			createFromPrototype.createFromPrototype("/component/definition.yaml",templateDefinitionFile,{"__name__":newComponentName,"__templateScript__":templateScriptFile.replace(packageJson.config.outputPath,""),"__dialog__":dialogDefinitionId});		
-			if(availabilityPatern){
-				addAvailability.addAvailabilityWhenCreated(templateDefinitionId,availabilityPatern);
+			createFromPrototype.createFromPrototype("/component/definition.yaml", templateDefinitionFile, {"__name__":newComponentName,"__templateScript__":templateScriptFile.replace(packageJson.config.outputPath,""),"__dialog__":dialogDefinitionId});		
+			if(availabilityPattern){
+				addAvailability.addAvailability(argumentParser.parseArguments(userArgs));
 			}
 		}
 		
@@ -37,14 +32,14 @@ var createComponent = function() {
 		if(!fs.existsSync(path.normalize(templateScriptFile))) {
 			createFromPrototype.createFromPrototype("/component/template.ftl",templateScriptFile,{"__name__":newComponentName});
 		} else {
-			console.log("WARN: ","'"+newComponentName+"' ["+templateScriptFile+"] templateScript already exists");
+			console.log("WARN: ","'" + newComponentName + "' [" + templateScriptFile + "] templateScript already exists");
 		}
 		
 		// dialog
 		if(!fs.existsSync(path.normalize(dialogDefinitionFile))) {
 			createFromPrototype.createFromPrototype("/component/dialog.yaml",dialogDefinitionFile,{"__name__":newComponentName});
 		} else {
-			console.log("WARN: ","'"+dialogDefinitionFile+"' dialog already exists");
+			console.log("WARN: ","'" + dialogDefinitionFile + "' dialog already exists");
 		}
 		
 	} else {
