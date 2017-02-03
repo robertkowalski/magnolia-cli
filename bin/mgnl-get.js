0fe9456f337b3dd1d34672991b35636f274c854f
Robert Kowalski <rok@kowalski.gd>
2017-02-03 10:13:59 +0000

    spike

--- bin/mgnl-get.js
+++ bin/mgnl-get.js
@@ -7,12 +7,25 @@ const program = require('../lib/commander_shimmed.js')
 const installLightModule = require('../lib/installLightModule.js')
 const packageJson = require('../package.json')
 
+const path = require('path')
+
 program
   .version(packageJson.version)
   .name('mgnl get')
-  .usage('<templateName>')
+  .usage('[--path]')
   .description('Gets a light module from npm')
+  .option('-p, --path <path>', 'The path to the light modules folder')
   .parse(process.argv)
 
-installLightModule(program.args[0])
+function getPath (p) {
+  if (!p) return process.cwd()
+
+  return path.resolve(program.path)
+}
+
+const opts = {
+  lightModulesFolder: getPath(program.path)
+}
+
+installLightModule(program.args[0], opts)
   .then(process.exit.bind(this, 0))
