61fb9f1367ff4867b30a1cfa24c54b82e6203d2a
Robert Kowalski <rok@kowalski.gd>
2017-03-06 13:34:57 +0200

    QA: ES6 - use const and arrow functions

--- test/createFromPrototypeTest.js
+++ test/createFromPrototypeTest.js
@@ -1,23 +1,24 @@
 /* eslint-env mocha */
-describe('createFromPrototype', function () {
-  var fs = require('fs-extra')
 
-  var testHelper = require('./testHelper')
-  var createFromPrototype = require('../lib/createFromPrototype')
+const fs = require('fs-extra')
+const expect = require('chai').expect
 
-  var expect = require('chai').expect
+const testHelper = require('./testHelper')
+const createFromPrototype = require('../lib/createFromPrototype')
 
-  it('should use custom prototypes if found in current directory or parent', function (done) {
+describe('createFromPrototype', () => {
+
+  it('should use custom prototypes if found in current directory or parent', (done) => {
     fs.mkdirsSync('test/destination/test-module/templates/pages')
-    var customPage = 'test/destination/test-module/templates/pages/custom.ftl'
-    var customContent = 'Hi, I am a custom template!'
+    const customPage = 'test/destination/test-module/templates/pages/custom.ftl'
+    const customContent = 'Hi, I am a custom template!'
 
     testHelper.invokeMgnlSubcommand('customize-local-config', '-p test/destination')
     fs.writeFileSync('test/destination/mgnl-cli-prototypes/page/template.ftl', customContent, 'utf-8')
 
     createFromPrototype.create('/page/template.ftl', customPage)
 
-    fs.readFile(customPage, 'utf-8', function (err, data) {
+    fs.readFile(customPage, 'utf-8', (err, data) => {
       expect(err).to.be.null
       expect(data).to.be.equal(customContent)
     })
